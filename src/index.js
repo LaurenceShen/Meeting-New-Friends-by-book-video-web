7dfde18cf5ef32c525bcae2e1b536a7055b9a113
LaurenceShen <laurence220016@gmail.com>
2023-07-12 14:21:43 +0800

    init

--- src/index.js
+++ src/index.js
@@ -1,17 +1,191 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+/* mdb-react-ui-kit */
+import 'mdb-react-ui-kit/dist/css/mdb.min.css';
+import "@fortawesome/fontawesome-free/css/all.min.css";
+import { MDBInputGroup, MDBInput, MDBIcon, MDBBtn } from 'mdb-react-ui-kit';
+import { MDBCol, MDBRow } from 'mdb-react-ui-kit';
+import {
+  MDBNavbar,
+  MDBContainer,
+  MDBNavbarNav,
+  MDBNavbarItem,
+  MDBNavbarLink,
+  MDBNavbarToggler,
+  MDBCollapse,
+  MDBDropdown,
+  MDBDropdownMenu,
+  MDBDropdownToggle,
+  MDBDropdownItem,
+} from 'mdb-react-ui-kit';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+function NavBar(){
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+  const [showNavRight, setShowNavRight] = useState(false);
+
+  return (
+    <MDBNavbar fixed="top" expand='sm' light >
+      <MDBContainer fluid>
+        <MDBNavbarToggler
+          type='button'
+          data-target='#navbarRightAlignExample'
+          aria-controls='navbarRightAlignExample'
+          aria-expanded='false'
+          aria-label='Toggle navigation'
+          onClick={() => setShowNavRight(!showNavRight)}
+        >
+          <MDBIcon icon='bars' fas />
+        </MDBNavbarToggler>
+
+        <MDBCollapse navbar show={showNavRight}>
+          <MDBNavbarNav right fullWidth={false} className='mb-2 mb-lg-0'>
+            <MDBNavbarItem>
+              <MDBNavbarLink active aria-current='page' href='#'>
+                Home
+              </MDBNavbarLink>
+            </MDBNavbarItem>
+            <MDBNavbarItem>
+              <MDBNavbarLink href='#'>Link</MDBNavbarLink>
+            </MDBNavbarItem>
+
+            <MDBNavbarItem>
+              <MDBDropdown>
+                <MDBDropdownToggle tag='a' className='nav-link'>
+                  Dropdown
+                </MDBDropdownToggle>
+                <MDBDropdownMenu>
+                  <MDBDropdownItem link>Action</MDBDropdownItem>
+                  <MDBDropdownItem link>Another action</MDBDropdownItem>
+                  <MDBDropdownItem link>Something else here</MDBDropdownItem>
+                </MDBDropdownMenu>
+              </MDBDropdown>
+            </MDBNavbarItem>
+            <MDBNavbarItem>
+              <MDBNavbarLink disabled href='#' tabIndex={-1} aria-disabled='true'>
+                Disabled
+              </MDBNavbarLink>
+            </MDBNavbarItem>
+          </MDBNavbarNav>
+        </MDBCollapse>
+      </MDBContainer>
+    </MDBNavbar>
+  );
+}
+
+function SearchBar(){
+
+  const [showSearchAlert, setShowSearchAlert] = useState(false);
+  return (
+    <div className = "Search-Block">
+    <div className = "Search-Column">
+      <MDBInputGroup>
+        <MDBInput label='Search' />
+      </MDBInputGroup>
+      </div>
+      <div className = "Search-Button">
+        <MDBBtn size='lg'  floating style={{ background: 'linear-gradient(to right, rgba(102, 126, 234, 0.5), rgba(118, 75, 162, 0.5))' }}>
+            <MDBIcon  icon='search' />
+        </MDBBtn>
+      </div>
+
+    </div>
+  );
+}
+
+class Recommendation extends React.Component {
+    render(){
+        return(
+            <div className = "Recommend">
+                <h3 className = "Title"> Recommend </h3>
+                <Books />
+            </div>
+        )
+    }
+}
+
+function Books(){
+    let imgs = [
+        [
+            'https://mdbootstrap.com/img/new/standard/city/041.jpg',
+            'https://mdbootstrap.com/img/new/standard/city/042.jpg',
+            'https://mdbootstrap.com/img/new/standard/city/043.jpg',
+        ],
+        [
+            'https://mdbootstrap.com/img/new/standard/city/044.jpg',
+            'https://mdbootstrap.com/img/new/standard/city/045.jpg',
+            'https://mdbootstrap.com/img/new/standard/city/046.jpg',
+        ],
+        [
+            'https://mdbootstrap.com/img/new/standard/city/047.jpg',
+            'https://mdbootstrap.com/img/new/standard/city/048.jpg',
+            'https://mdbootstrap.com/img/new/standard/city/049.jpg',
+        ],
+        [
+            'https://mdbootstrap.com/img/new/standard/city/050.jpg',
+            'https://mdbootstrap.com/img/new/standard/city/051.jpg',
+            'https://mdbootstrap.com/img/new/standard/city/052.jpg',
+        ],
+    ]
+
+    let imgs_post = imgs.map((i)=>
+        <MDBRow>
+        <MDBCol lg='4' md='12' className='mb-4'>
+          <img
+            src={i[0]}
+            className='img-fluid shadow-2-strong rounded-4'
+            alt=''
+          />
+        </MDBCol>
+        <MDBCol lg='4' md='6' className='mb-4'>
+          <img
+            src={i[1]}
+            className='img-fluid shadow-2-strong rounded-4'
+            alt=''
+          />
+        </MDBCol>
+        <MDBCol lg='4' md='6' className='mb-4'>
+          <img
+            src={i[2]}
+            className='img-fluid shadow-2-strong rounded-4'
+            alt=''
+          />
+        </MDBCol>
+        </MDBRow>
+    )
+    console.log(imgs_post)
+  return (
+    <MDBContainer>
+        {imgs_post}
+    </MDBContainer>
+  )
+}
+
+class FrontPage extends React.Component {
+    render(){
+        return(
+            <div className = "FrontPage">
+                <div className = "Nav-Bar">
+                    <NavBar />
+                </div>
+                <h2 className = "Title"> Search! </h2>
+                <div className = "Search-Bar">
+                    <SearchBar />
+                </div>
+                <div className = "Recommend-Block">
+                    <Recommendation 
+                    />
+                </div>
+            </div>
+        );
+    }
+}
+
+
+
+
+
+// ========================================
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<FrontPage />);
