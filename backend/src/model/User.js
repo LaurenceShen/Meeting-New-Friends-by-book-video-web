f30ffab62b8dc0fe4e98b49dc99010e582742b37
LaurenceShen <laurence220016@gmail.com>
2023-08-30 12:05:39 +0800

    :)

--- backend/src/model/User.js
+++ backend/src/model/User.js
@@ -3,6 +3,10 @@ const Schema = mongoose.Schema;
 const UserSchema = new Schema({
  name: String,
  email: String,
+ profile: String,
+ Photos: [String],
+ Followers: [String],
+ Following: [String],
 });
 const User = mongoose.model('User', UserSchema);
 export default User; 
